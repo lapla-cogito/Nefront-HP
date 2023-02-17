2cbf67c212c15fe78671556e94e40be0ba3407a6
laplaciancpp <kakikei20021108@gmail.com>
2023-02-17 18:02:35 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = {
   images: {
     loader: "akamai",
     path: "",
+    domains: ["https://laplaciancpp.github.io"]
   },
   // assetPrefix: process.env.NODE_ENV === "production" ? "/Nefront-HP" : "",
   "process.env.BACKEND_URL": prod ? "/Nefront-HP" : "",
