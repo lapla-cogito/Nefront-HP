7deb3910297ee5df98a2f05ec3e7ba1af434c665
laplaciancpp <kakikei20021108@gmail.com>
2023-09-29 14:58:28 +0900

    Add padding to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import { Box } from '@chakra-ui/react';
 const Footer = () => {
     return (
         // @ts-ignore
-        <Box align="center" opacity={0.4} fontSize="sm">
+        <Box align="center" opacity={0.4} fontSize="sm" py={8}>
             &copy; {new Date().getFullYear()} Nefront Inc. All Rights Reserved.
         </Box>
     );
